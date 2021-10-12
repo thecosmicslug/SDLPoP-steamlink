809d9146d0c781f55b85507b908199ab90a5ec68
TheCosmicSlug <thecosmicslug@gmail.com>
2021-10-12 15:14:49 +0100

    Build script now creates an installation .tgz for Steam Link.

--- build_sdlpop_steamlink.sh
+++ build_sdlpop_steamlink.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 #
-echo "Checking For SDK..."
-TOP=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-if [ "${MARVELL_SDK_PATH}" = "" ]; then
-	echo "Setting up SDK..."
-	MARVELL_SDK_PATH="$(cd "${TOP}/../.." && pwd)"
-fi
-if [ "${MARVELL_ROOTFS}" = "" ]; then
-	source "${MARVELL_SDK_PATH}/setenv.sh" || exit 1
-fi
-cd "${TOP}"
+echo "Checking For Steam Link SDK..."
 source ../../setenv.sh
 
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+
+echo "Preparing for Build..."
+rm $parent_path/output/SDLPoP-steamlink/prince
+rm $parent_path/output/SDLPoP-steamlink.tgz
+
 cd src
-echo "Building SDLPoP-steamlink... (Release)"
+echo "Building SDLPoP-steamlink..."
+make clean
 make -f Makefile.steamlink || exit 2
 
-echo "Build Complete!"
+echo "Creating Installation Package..."
+cd ..
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+mv $parent_path/prince $parent_path/output/prince
+cp $parent_path/output/prince $parent_path/output/SDLPoP-steamlink/prince
+
+# Pack it up
+export DESTDIR="${PWD}/output/SDLPoP-steamlink/"
+cd output
+
+name=$(basename ${DESTDIR})
+tar zcvf $name.tgz $name || exit 3
+
+echo "Build Complete! Check in /output/ directory."
 
